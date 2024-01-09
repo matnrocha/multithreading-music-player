c4ac30fcf1afe9d1b079ec50b1a4f8e5facfc3e6
Gust4voSSM <gugu9santiago@gmail.com>
2024-01-09 03:16:44 -0300

    Implemented adding songs, removing songs, playing and stopping songs. Commented on some methods.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,17 @@
 import javazoom.jl.decoder.*;
 import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.FactoryRegistry;
 import support.PlayerWindow;
-
+import support.Playlist;
+import support.Song;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Player {
 
@@ -23,13 +29,99 @@ public class Player {
     private AudioDevice device;
 
     private PlayerWindow window;
+    private Playlist playlist;
     private int currentFrame = 0;
+    private Song currentSong;
+    private boolean playing;
+    private final Lock lock = new ReentrantLock();
+    private final Condition songStopped = lock.newCondition();
 
-    private final ActionListener buttonListenerPlayNow = e -> {};
-    private final ActionListener buttonListenerRemove = e -> {};
-    private final ActionListener buttonListenerAddSong = e -> {};
+    /** Each new track played is instantiated as a Thread object that uses this method call as parameter.
+     *  When a new track thread starts, it will wait for the previous track to stop and signal the condition.
+     */
+    private void trackThread() {
+        playing = true;
+        boolean EOF = false;
+        if (decoder != null) {
+            closeBitStream();
+        }
+        try {
+            createBitStream();
+            while (playing && !EOF) {
+                EOF = !playNextFrame();
+            }
+            lock.lock();
+            songStopped.signal();
+            lock.unlock();
+        } catch (FileNotFoundException | JavaLayerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /** Closes the bit stream and the audio device */
+    private void closeBitStream(){
+        try{
+            bitstream.close();
+        } catch (BitstreamException e) {
+            throw new RuntimeException(e);
+        }
+        device.close();
+    }
+
+    /**Create a new audio device as well as a new input stream
+     *
+     * @throws JavaLayerException
+     * @throws FileNotFoundException
+     */
+    private void createBitStream() throws JavaLayerException, FileNotFoundException {
+        device = FactoryRegistry.systemRegistry().createAudioDevice();
+        device.open(decoder = new Decoder());
+        bitstream = new Bitstream(currentSong.getBufferedInputStream());
+    }
+
+    /** Stop any song from playing and resets the GUI
+     *
+     */
+    private void stopSong() {
+        playing = false;
+        EventQueue.invokeLater(() -> window.resetMiniPlayer());
+    }
+
+    private final ActionListener buttonListenerPlayNow = e -> new Thread(() ->{
+        int index = window.getSelectedSongIndex();
+        playlist.setCurrentIndex(index);
+        currentSong = playlist.get(index);
+        EventQueue.invokeLater(() -> {
+            window.setEnabledStopButton(true);
+            window.setEnabledPlayPauseButton(true);
+            window.setPlayPauseButtonIcon(window.BUTTON_ICON_PAUSE);
+        });
+        lock.lock();
+        try {
+            if (playing) {
+                playing = false;       // Signals to the previous thread to stop reproduction;
+                songStopped.await();   // Then waits for it to stop;
+            }
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            lock.unlock();
+        }
+        new Thread(this::trackThread).start();
+
+    }).start();
+    private final ActionListener buttonListenerRemove = e -> new Thread(() -> {
+        int index = window.getSelectedSongIndex();
+        if (index == playlist.getCurrentIndex()) stopSong();
+        playlist.remove(index);
+        updateWindow();
+    }).start();
+    private final ActionListener buttonListenerAddSong = e -> new Thread(() -> {
+        Song newSong = window.openFileChooser();
+        playlist.add(newSong);
+        updateWindow();
+    }).start();
     private final ActionListener buttonListenerPlayPause = e -> {};
-    private final ActionListener buttonListenerStop = e -> {};
+    private final ActionListener buttonListenerStop = e -> new Thread(this::stopSong).start();
     private final ActionListener buttonListenerNext = e -> {};
     private final ActionListener buttonListenerPrevious = e -> {};
     private final ActionListener buttonListenerShuffle = e -> {};
@@ -48,15 +140,23 @@ public class Player {
         }
     };
 
+    private void updateWindow() {
+        EventQueue.invokeLater(() -> window.setQueueList(playlist.getDisplayInfo()));
+    }
+
     public Player() {
-        String[][] queue = { // Example table
-                {"name1",   "album1",   "artist1",  "year1",    "length1"   },
-                {"name2",   "album2",   "artist3",  "year3",    "length3"   },
-                {"...",     "...",      "...",      "...",      "..."       }
-        };
+        this.playlist = new Playlist();
+
+        try {
+            this.device = FactoryRegistry.systemRegistry().createAudioDevice();
+        } catch (JavaLayerException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[][] table = playlist.getDisplayInfo();
         EventQueue.invokeLater(() -> window = new PlayerWindow(
                 "Player", // Placeholder title
-                queue,
+                table,
                 buttonListenerPlayNow,
                 buttonListenerRemove,
                 buttonListenerAddSong,
